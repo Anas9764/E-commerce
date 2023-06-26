4cdb78b94e00d080b8197843a44ea1009028aa13
Anas Qureshi <85098894+Anas9764@users.noreply.github.com>
2023-06-26 07:06:03 +0000

    higher order fuction for controller

---
+++ src/models/coupon.schema.js
@@ -0,0 +1,21 @@
+import mongoose, { models } from "mongoose";
+
+const couponSchema = new mongoose.Schema(
+    {
+code:{
+    type:String,
+    required:[true,"please provide a coupon code"]
+},
+discount:{
+    type:Number,
+    default:0
+}, 
+active:{
+    type: Boolean,
+    default:true
+}
+    },{timestamps:true}
+
+)
+
+export default mongoose.model("Coupon",couponSchema)
