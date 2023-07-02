58e528343d0b1b2ac064c0879110d04eec5ebd57
Anas Qureshi <85098894+Anas9764@users.noreply.github.com>
2023-07-02 18:03:59 +0000

    added login and logout in controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,96 @@
+// signup a new user
+import asyncHandler from "../service/asyncHandler";
+import CustomError from "../utils/CustomError";
+import User from "../models/user.js"
+
+
+export const cookieOptions = {
+    expires: new Date(Date.now()+ 3 * 24 * 60 * 60 * 1000),
+    httpOnly:true
+}
+
+export const signUp = asyncHandler(async (req, res) => {
+    // get data from user
+    const { name, email, password } = req.body
+
+    // validation
+    if (!name || !email || !password) {
+        // res.status(400).json({
+        //     success: false,
+        //     message
+        // })
+
+        // throw new Error('Required fills ')
+        throw new CustomError("Please add all fields", 400)
+    }
+
+    // lets add this data to database
+
+    // check if user already exists 
+    const existingUSer = await User.findOne({ email })
+
+    if (existingUSer) {
+        throw new CustomError("User already exists", 400)
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    const token = user.getJWTtoken()
+    // safety
+    user.password = undefined
+
+    // store this token in user's cookie
+    res.cookie("token",token,cookieOptions)
+
+    // send back a response to user
+res.status(200).json({
+    success: true,
+     token,
+     user,
+})
+})
+
+export const login = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    // validation
+    if (!email || !password){
+        throw new CustomError("Please fill all details",400)
+            }
+
+            const user = User.findOne({email}).select("+password")
+
+        if (!user){
+            throw new CustomError("Invalid credentials", 400)
+        }
+
+      const isPasswordMatched = await  user.comparePassword(password)
+
+      if (isPasswordMatched){
+const token = user.getJWTtoken()
+user.password = undefined
+res.cookie("token",token, cookieOptions)
+return res.status(200).json({
+    success:true,
+    token,
+    user
+})
+
+      }
+
+      throw new CustomError("Password is incorrect", 400)
+} )
+
+export const logout = asyncHandler(async (req , res) =>{
+    res.cookie("token" , null ,{
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+    res.status(200).json({
+        success:true,
+        message : 'Logged Out'
+    })
+})
